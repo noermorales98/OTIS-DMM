aa7b8ac89d37ce5756df5fd7aafdb3fff54ef509
noeli <noe.rmorales98@gmail.com>
2022-03-02 09:32:25 -0500

    ventana principal sin css

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -6,7 +6,18 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  tareas: Tareas = {
+    id: 1,
+    name: 'Noeli',
+    total: 10,
+  };
 
   constructor() {}
 
+}
+
+export interface Tareas {
+  id: number;
+  name: string;
+  total: number;
 }
