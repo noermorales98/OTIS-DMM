8f7dc4479f4b90a10506679aa59f82121133855d
lorddeivis <fragosomadera99@gmail.com>
2022-03-04 21:46:19 -0500

    jijijija

--- src/app/pages/add-task/add-task.page.ts
+++ src/app/pages/add-task/add-task.page.ts
@@ -13,3 +13,23 @@ export class AddTaskPage implements OnInit {
   }
 
 }
+export class MiComponente {
+  //Esto se lo pasamos a la vista.
+  agnosPersonalizados = [2020, 2016, 2008, 2004, 2000, 1996];
+  nombresDiasCortados = ['lun', 'mar', 'mie', 'jue', 'vie', 'sab', 'dom'];
+  opcionesPersonalizas: any;
+  constructor() {
+    this.opcionesPersonalizas = {
+      buttons: [{
+        text: 'Save',
+        handler: () => console.log('Clicked Save!')
+      }, {
+        text: 'Log',
+        handler: () => {
+          console.log('Clicked Log. Do not Dismiss.');
+          return false;
+        }
+      }]
+    }
+  }
+}
